31b764d091842832321d8182727035063a92c716
isaias-silva <zackblackmangold@gmail.com>
2022-01-24 21:07:15 -0300

    webdriver evaluate

--- modules/automata.js
+++ modules/automata.js
@@ -1,43 +1,55 @@
-const webdriver=require('selenium-webdriver')
-const readline=require('readline-sync')
-async function web(){
+const webdriver = require('selenium-webdriver')
+const readline = require('readline-sync')
+async function web() {
 
-    let mail=readline.question('nome de usuário: ')
-    let pass=readline.question('senha: ',)
+    let mail = readline.question('nome de usuário: ')
+    let pass = readline.question('senha: ',)
     const capabilities = {
-        browserName: 'firefox'
-        
-       
-        
+        browserName: 'chrome'
+
+
+
     }
     const driver = await new webdriver.Builder().withCapabilities(capabilities).build()
     await driver.get("https://www.instagram.com/");
     await driver.sleep(4000)
     await driver.findElement(webdriver.By.xpath('//*[@name="username"]')).sendKeys(mail, webdriver.Key.LEFT);
     await driver.findElement(webdriver.By.xpath('//*[@name="password"]')).sendKeys(pass, webdriver.Key.ENTER);
-    pass=''
+    pass = ''
     await driver.sleep(4000)
     driver.get(`https://www.instagram.com/${mail}`)
-     let desc=  await driver.findElement(webdriver.By.className("QGPIr")).getText()
-    desc=desc.split('\n')
-for(let i in desc){
- 
-    await driver.findElement(webdriver.By.xpath('//*[@aria-label="Entrada da pesquisa"]')).sendKeys(desc[i], webdriver.Key.ENTER);
-   await driver.sleep(4000)
-   const alvo= await driver.findElements(webdriver.By.className('-qQT3'))
+    let desc = await driver.findElement(webdriver.By.className("QGPIr")).getText()
+    desc = desc.split('\n')
+    for (let i in desc) {
+        await driver.findElement(webdriver.By.xpath('//*[@aria-label="Entrada da pesquisa"]')).sendKeys(desc[i], webdriver.Key.ENTER);
+        await driver.sleep(4000)
+        const alvo = await driver.findElements(webdriver.By.className('-qQT3'))
 
-    await alvo[0].click()
-    await driver.sleep(2000)
-    const folo= await driver.findElements(webdriver.By.className('-nal3'))
-    await folo[1].click()
-  
-    await driver.sleep(2000)
-    await driver.navigate().back()
-    
-}
+        await alvo[0].click()
+        await driver.sleep(2000)
+        const folo = await driver.findElements(webdriver.By.className('-nal3'))
+        await folo[1].click()
+        await driver.sleep(4000)
+        const followers = await driver.findElements(webdriver.By.xpath('//button[@class="sqdOP  L3NKy   y3zKF     "]'))
+        const followers2 = await driver.findElements(webdriver.By.xpath('//button[@class="sqdOP  L3NKy    _8A5w5    "]'))
+        try{
+        for(let x in followers){
+           await followers[x].click()
+           await driver.sleep(1000)
+        }
+    }catch{
+        for(let y in followers2){
+            await followers2[y].click()
+            await driver.sleep(1000)
+         }
+    }
+        await driver.navigate().back()
+        await driver.navigate().back()
+
+    }
 
 
-    
-  
+
+
 }
 web()
